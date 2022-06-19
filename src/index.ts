3ea5bfb4decc964325f7cf57cd47fd21dc63a732
Vladimir Bessonov <bessmy@gmail.com>
2022-06-19 20:20:44 +0400

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import http from 'http';
+import { deleteUser } from './routes/deleteUser.js';
+import { get } from './routes/get.js';
+import { nonExist } from './routes/non-exist.js';
+import { postUser } from './routes/postUser.js';
+import { putUser } from './routes/putUser.js';
+import { sendServerErrorResponse } from './utils.js';
+
+const server = http
+  .createServer((req, res) => {
+    try {
+      switch (req.method) {
+        case 'GET':
+          get(req, res);
+          break;
+
+        case 'POST':
+          postUser(req, res);
+          break;
+
+        case 'PUT':
+          putUser(req, res);
+          break;
+
+        case 'DELETE':
+          deleteUser(req, res);
+          break;
+
+        default:
+          nonExist(req, res);
+          break;
+      }
+    } catch {
+      sendServerErrorResponse(res);
+    }
+  })
+  .listen(8080);
+
+server.on('error', (err) => {
+  console.error(err);
+});
+
+console.log('Server running at http://localhost:8080/');
